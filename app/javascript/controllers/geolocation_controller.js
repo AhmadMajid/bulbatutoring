8bc2e02cecc1a364160167984ac61501a8c5fda5
Ahmad Majid <Ahmadmajid1994@gmail.com>
2022-08-27 22:30:56 +0100

    Calculate distance with geolocation not logged in

--- app/javascript/controllers/geolocation_controller.js
+++ app/javascript/controllers/geolocation_controller.js
@@ -1,10 +1,23 @@
 import { Controller } from '@hotwired/stimulus'
+import { getDistance, convertDistance } from 'geolib';
 
 export default class extends Controller {
+  static targets = ['tutor'];
+
   connect() {
     window.navigator.geolocation.getCurrentPosition((position) => {
       this.element.dataset.latitude = position.coords.latitude;
       this.element.dataset.longitude = position.coords.longitude;
+
+
+      this.tutorTargets.forEach((tutorTarget) => {
+        let distanceFrom = getDistance(
+          { latitude: position.coords.latitude, longitude: position.coords.longitude },
+          { latitude: tutorTarget.dataset.latitude, longitude: tutorTarget.dataset.longitude },
+        );
+
+        tutorTarget.querySelector('[data-distance-away]').innerHTML = `${Math.round(convertDistance(distanceFrom, 'km'))} kilometers away`;
+      });
     });
   }
 }
