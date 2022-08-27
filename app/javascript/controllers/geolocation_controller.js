b2b985c38174ac13546cb483a65865b105adfac6
Ahmad Majid <Ahmadmajid1994@gmail.com>
2022-08-28 00:23:39 +0100

    Refactoring geolocation controller

--- app/javascript/controllers/geolocation_controller.js
+++ app/javascript/controllers/geolocation_controller.js
@@ -8,27 +8,34 @@ export default class extends Controller {
   connect() {
     if (isEmpty(this.element.dataset.latitude) && isEmpty(this.element.dataset.longitude)) {
       window.navigator.geolocation.getCurrentPosition((position) => {
-        this.element.dataset.latitude = position.coords.latitude;
-        this.element.dataset.longitude = position.coords.longitude;
-
-        this.tutorTargets.forEach((tutorTarget) => {
-          let distanceFrom = getDistance(
-            { latitude: position.coords.latitude, longitude: position.coords.longitude },
-            { latitude: tutorTarget.dataset.latitude, longitude: tutorTarget.dataset.longitude },
-          );
-
-          tutorTarget.querySelector('[data-distance-away]').innerHTML = `${Math.round(convertDistance(distanceFrom, 'km'))} kilometers away`;
-        });
+        this.setUserCoordinates(position.coords);
+        this.setDistanceText()
       });
     } else {
-      this.tutorTargets.forEach((tutorTarget) => {
-        let distanceFrom = getDistance(
-          { latitude: this.element.dataset.latitude, longitude: this.element.dataset.longitude },
-          { latitude: tutorTarget.dataset.latitude, longitude: tutorTarget.dataset.longitude },
-        );
-
-        tutorTarget.querySelector('[data-distance-away]').innerHTML = `${Math.round(convertDistance(distanceFrom, 'km'))} kilometers away`;
-      });
+      this.setDistanceText();
     }
+  }
+
+  setUserCoordinates(coordinates) {
+    this.element.dataset.latitude = coordinates.latitude;
+    this.element.dataset.longitude = coordinates.longitude;
+  }
+
+  getUserCoordinates() {
+    return {
+      latitude: this.element.dataset.latitude,
+      longitude: this.element.dataset.longitude,
+    };
+  }
+
+  setDistanceText() {
+    this.tutorTargets.forEach((tutorTarget) => {
+      let distanceFrom = getDistance(
+        this.getUserCoordinates(),
+        { latitude: tutorTarget.dataset.latitude, longitude: tutorTarget.dataset.longitude },
+      );
+
+      tutorTarget.querySelector('[data-distance-away]').innerHTML = `${Math.round(convertDistance(distanceFrom, 'km'))} kilometers away`;
+    });
   }
 }
