23b271bc5114c304241dcc3e851b78d8d503419c
Ahmad Majid <Ahmadmajid1994@gmail.com>
2022-08-27 17:06:49 +0100

    This adds stimulus controller for user auth forms in modals

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "home#index"
+
+  namespace :api do
+    resources :users, only: :show
+    get "/users_by_email" => "users_by_emails#show", as: :users_by_email
+  end
 end
