a5eba1a7233bb98c6578916ee0f7d218a1985f16
Ahmad Majid <Ahmadmajid1994@gmail.com>
2022-08-28 10:49:58 +0100

    Add images to tutors

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,14 @@
-10.times do
-  Tutor.create!(
+10.times do |i|
+  tutor = Tutor.create!(
     name: Faker::Lorem.word,
     headline: Faker::Lorem.sentence,
     description: Faker::Lorem.paragraph,
     address_1: Faker::Address.street_address,
     city: Faker::Address.city,
     state: Faker::Address.state,
-    country: "United States"
+    country: "United States",
+    price: Money.from_amount((25..100).to_a.sample)
   )
+
+  tutor.images.attach(io: File.open(Rails.root.join("db", "sample", "images", "tutor_#{i + 1}.png")), filename: tutor.name)
 end
