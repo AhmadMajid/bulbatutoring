8de805d2d4d673970710578e7b3c74a2dfca3aed
Ahmad Majid <Ahmadmajid1994@gmail.com>
2022-08-28 09:57:31 +0100

    Add money-rails gem to tutor model

---
+++ db/migrate/20220618143752_add_price_cents_to_tutors.rb
@@ -0,0 +1,5 @@
+class AddPriceCentsToTutors < ActiveRecord::Migration[7.0]
+  def change
+    add_monetize :tutors, :price, amount: { null: true, default: nil }, currency: { null: true, default: nil }
+  end
+end
