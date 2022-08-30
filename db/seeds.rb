7c426029a7c91e0860aa01a76663d828c9223580
Ahmad Majid <Ahmadmajid1994@gmail.com>
2022-08-31 00:51:16 +0100

    less seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 user_pictures = []
 
-6.times do
+1.times do
   user_pictures << URI.parse(Faker::LoremFlickr.image).open
 end
 
@@ -9,13 +9,13 @@ me.profile.update(first_name: "John", last_name: "Smith")
 me.profile.picture.attach(io: user_pictures[0], filename: "#{me.full_name}.jpg")
 
 
-5.times do |i|
+1.times do |i|
   user = User.create(email: Faker::Internet.email, password: "password")
   user.profile.update(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
   user.profile.picture.attach(io: user_pictures[i + 1], filename: "#{user.full_name}.jpg")
 end
 
-10.times do |i|
+1.times do |i|
   tutor = Tutor.create!(
     user: me,
     name: Faker::Lorem.word,
