cd68806ef61e4be26c418e42bba5e5ca0941266f
Ahmad Majid <Ahmadmajid1994@gmail.com>
2023-04-04 08:54:37 +0100

    Add model specs for Review model

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -9,4 +9,20 @@ RSpec.describe Review, type: :model do
   it { should validate_numericality_of(:rating).is_greater_than_or_equal_to(1) }
   it { should validate_numericality_of(:rating).is_less_than_or_equal_to(5) }
   it { should validate_numericality_of(:rating).only_integer }
+
+  describe "#update_average_rating" do
+    let(:tutor) { create(:tutor) }
+    let(:user) { create(:user) }
+
+    it "updates the tutor's average_rating" do
+      Review.create(title: "New Review", body: "New Review Body", rating: 5, reviewable: tutor, user: user)
+      expect(tutor.reload.average_rating).to eq 5
+    end
+
+    it "updates the tutor's average_rating properly when there are two reviews" do
+      Review.create(title: "New Review", body: "New Review Body", rating: 5, reviewable: tutor, user: user)
+      Review.create(title: "New Review 2", body: "New Review Body 2", rating: 3, reviewable: tutor, user: user)
+      expect(tutor.reload.average_rating).to eq 4
+    end
+  end
 end
