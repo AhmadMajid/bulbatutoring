350d9654e59bd69aa1bd183d07b63e9dff96cfb0
Ahmad Majid <Ahmadmajid1994@gmail.com>
2023-04-04 09:10:20 +0100

    Happy and sad paths for ProfileController

--- spec/requests/profiles_spec.rb
+++ spec/requests/profiles_spec.rb
@@ -7,23 +7,46 @@ RSpec.describe "Profiles", type: :request do
   before { sign_in user }
 
   describe "GET show" do
-    it "succeeds" do
-      get profile_path(profile)
-      expect(response).to be_successful
+    context "with valid profile id" do
+      it "succeeds" do
+        get profile_path(profile)
+        expect(response).to be_successful
+      end
+    end
+
+    context "with invalid profile id" do
+      it "fails" do
+        expect { get profile_path(0) }.to raise_error(ActiveRecord::RecordNotFound)
+      end
     end
   end
 
   describe "PUT update" do
-    it "succeeds" do
-      profile.update! first_name: "Foo", last_name: "Bar"
+    context "with valid update params" do
+      it "succeeds" do
+        profile.update! first_name: "Foo", last_name: "Bar"
 
-      put profile_path(profile), params: {
-        profile: {
-          first_name: "John"
+        put profile_path(profile), params: {
+          profile: {
+            first_name: "John"
+          }
         }
-      }
-      expect(profile.reload.first_name).to eq("John")
-      expect(response).to be_redirect
+        expect(profile.reload.first_name).to eq("John")
+        expect(response).to be_redirect
+      end
+    end
+
+    context "with invalid update params" do
+      it "does not update the user_id" do
+        profile.update! first_name: "Foo", last_name: "Bar"
+
+        put profile_path(profile), params: {
+          profile: {
+            user_id: nil
+          }
+        }
+        expect(profile.reload.user_id).to_not be nil
+      end
     end
   end
 end
